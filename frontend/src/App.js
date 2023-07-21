8765c367f80a1cf5f7e41529ff6553663cab8f34
aditya <adityasuman990@gmail.com>
2023-07-22 00:06:28 +0530

    features added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Home from "./Pages/Home";
+// import Features from "./Components/Features_Card";
 function App() {
   return (
     <>
       <Home />
+      {/* <Features/> */}
     </>
   );
 }
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from "../Components/Navbar"
+import Features from "../Components/Features_Card";
 const Home = () => {
   return (
     <>
-    <div>Home</div>
+    {/* <div>Home</div> */}
     <Navbar />
+    <Features/>
     </>
   )
 }
