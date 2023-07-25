4a4b9141b4e85fc20615298d145811b18b13352b
aditya <adityasuman990@gmail.com>
2023-07-25 10:41:48 +0530

    carousel added

---
+++ src/Components/Carousel/Carousel.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const CarouselExample = () => {
+  return (<>
+    <div className='row mt-5'>
+        <div className='col-12 '>
+    <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
+      <div className="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img src="https://images.pexels.com/photos/17161093/pexels-photo-17161093/free-photo-of-lilac-flower-petals-ingredient-template-beautiful-lilac-flowers-spring.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" className="d-block w-100" alt="..." />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>First slide label</h5>
+            <p>Some representative placeholder content for the first slide.</p>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img src="https://images.pexels.com/photos/610293/pexels-photo-610293.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="d-block w-100" alt="..." />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>Second slide label</h5>
+            <p>Some representative placeholder content for the second slide.</p>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img src="https://images.pexels.com/photos/1851481/pexels-photo-1851481.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="d-block w-100" alt="..." />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>Third slide label</h5>
+            <p>Some representative placeholder content for the third slide.</p>
+          </div>
+        </div>
+      </div>
+      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
+    </div>
+    </div>
+    </>
+  );
+};
+
+export default CarouselExample;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,12 +4,14 @@ import Features from "../Components/FeaturesCard/Features_Card";
 import Timeline from "../Components/Timeline/Timeline";
 import Footer from "../Components/Footer/Footer";
 import Cards2 from "../Components/card2/Cards2";
+import  Carousel  from "../Components/Carousel/Carousel";
 // import Newletter from "../Components/Newsletter";
 const Home = () => {
   return (
     <>
     {/* <div>Home</div> */}
     <Navbar />
+    <Carousel/>
     <Features/>
     <Timeline/>
     <Cards2/>
