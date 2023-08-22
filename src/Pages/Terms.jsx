2602c2fe6f12d397da255cb6a95b0d2512ee6c39
adityasuman <adityasuman990@gmial.com>
2023-08-22 23:24:00 +0530

    bug fixed

--- src/Pages/Terms.jsx
+++ src/Pages/Terms.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 // import Terms1 from "../Components/Signup/SignTerms";
 // import Info from "../Pages/info/Info";
-// import Compound from "../Pages/Calculators/Compound";
+import Compound from "../Pages/Calculators/Compound";
 import Income from "../Pages/Calculators/Incometax";
 const Terms = () => {
   return (
     <>
      <Income/>
+     <Compound/>
     </>
   )
 }
